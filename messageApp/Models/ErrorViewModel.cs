da62e62b3adfc7efd347e23f0bb7b749bd6e01ea
di-nethra <anjana.dinethra@gmail.com>
2021-10-09 18:29:44 +0530

    initial commit

---
+++ messageApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace messageApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
