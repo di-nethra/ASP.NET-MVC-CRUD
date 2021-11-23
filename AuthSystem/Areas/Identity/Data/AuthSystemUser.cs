6ce17c2eb6492e4a7bafd375cf219727731b1872
di-nethra <anjana.dinethra@gmail.com>
2021-11-23 16:21:23 +0530

    Login and reg added

---
+++ AuthSystem/Areas/Identity/Data/AuthSystemUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthSystem.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the AuthSystemUser class
+    public class AuthSystemUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName ="varchar(100)") ]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "varchar(100)")]
+        public string LastName { get; set; }
+
+    }
+}
---
+++ AuthSystem/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AuthSystem.Areas.Identity.Data;
+using AuthSystem.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthSystem.Areas.Identity.IdentityHostingStartup))]
+namespace AuthSystem.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<AuthSystemUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
